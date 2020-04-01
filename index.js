63da8911bda1681d666eef961be7a351cdf62202
Jonghyun <wdh008@naver.com>
2020-04-01 18:41:34 +0900

    update 2020-04-01

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 //백엔드의 시작점 
-
 const express = require('express');
 const app = express();
 const port = 3000;
+const bodyParser = require('body-parser');
+const { User } = require("./models/User");
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://jongmato:jonghyun@youtubeclone-amn98.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true,
@@ -12,6 +13,22 @@ mongoose.connect('mongodb+srv://jongmato:jonghyun@youtubeclone-amn98.mongodb.net
 }).then(()=> console.log('MongoDB Connected...'))
   .catch(err => console.log(err));
 
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+//application/json type 을 가져온다.
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => res.send(''));
+
+app.post('/register', (req,res) => {
+    //회원가입시 필요한 정보들을 client에서 가져와 DB에 넣어준다.
+    const user = new User(req.body);
+    user.save((err, userInfo) => {
+        if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        });
+    });
+});
 
 app.listen(port,() => console.log(`Example app listening on port ${port}!`));
