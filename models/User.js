35bfbcce910b1f60e6223046339963b08225c25a
Jonghyun <wdh008@naver.com>
2020-04-02 15:55:33 +0900

    token create

--- models/User.js
+++ models/User.js
@@ -1,4 +1,9 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+const jwt = require('jsonwebtoken');
+
+
 const userSchema = mongoose.Schema({
     name: {
         type: String,
@@ -29,6 +34,49 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 });
+
+userSchema.pre('save', function(next){
+    var user = this;
+
+    if(user.isModified('password')) {
+        //비밀번호를 암호화 시킨다.
+                bcrypt.genSalt(saltRounds, function(err, salt) {
+                if(err) return next(err)
+
+                bcrypt.hash(user.password, salt, function(err, hash) {
+                // Store hash in your password DB.
+                //hash = 암호화된 비밀번호
+                if(err) return next(err)
+
+                user.password = hash;
+                next();
+            });
+        });
+    }else {
+        next();
+    }
+});
+
+userSchema.methods.comparePassword = function(plainPassword, cb) {
+    //plainPassword 와 암호화된 비밀번호가 맞는지 확인
+    bcrypt.compare(plainPassword, this.password, function(err,isMatch) {
+        if(err) return cb(err),
+            cb(null, isMatch)
+    });
+};
+
+userSchema.methods.generateToken = function(cb) {
+    var user = this;
+    //jsonwebtoken 라이브러리를 이용해 token 생성
+    var token = jwt.sign(user._id.toHexString(), 'secretToken');
+
+    user.token = token;
+    user.save(function(err, user) {
+        if(err) return cb(err)
+        cb(null, user)
+    });
+};
+
 
 const User = mongoose.model('User',userSchema );
 
