f6970c18bfd29892ba93e3b5ab16be5b9de6ff6c
Jonghyun <wdh008@naver.com>
2020-04-03 18:17:30 +0900

    login page finish

--- client/src/components/Views/LoginPage/LoginPage.js
+++ client/src/components/Views/LoginPage/LoginPage.js
@@ -1,9 +1,64 @@
-import React from 'react';
+import React ,{ useState } from 'react';
+import Axios from 'axios'
+import { useDispatch } from 'react-redux';
+import { loginUser } from '../../../_actions/user_action';
+import { withRouter } from 'react-router-dom';
 
-function LoginPage() {
+function LoginPage(props) {
+    const dispatch = useDispatch();
+
+    const [Email, setEmail] = useState("")
+    const [Password, setPassword] = useState("")
+
+    const onEmailHandler = (event) => {
+        setEmail(event.currentTarget.value)
+    }
+
+    const onPasswordHandler = (event) => {
+        setPassword(event.currentTarget.value)
+    }
+
+    const onSubmitHandler = (event) => {
+        event.preventDefault(); //page refresh 방지를 위한 method
+
+        let body = {
+            email: Email,
+            password: Password
+        }
+
+        dispatch(loginUser(body))
+            .then(response => {
+                if (response.payload.loginSuccess) {
+                    props.history.push('/') //LandingPage로 이동 .push('경로') 지정된 경로로 이동
+                } else {
+                    alert('Error˝')
+                }
+            })
+
+
+    }
+
+
     return (
-        <div>
-            LoginPage
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center'
+            , width: '100%', height: '100vh'
+        }}>
+            
+            <form style={{ display: 'flex', flexDirection: 'column' }}
+                onSubmit={onSubmitHandler}
+            >
+                <label>Email</label>
+                <input type="email" value={Email} onChange={onEmailHandler} />
+                <label>Password</label>
+                <input type="password" value={Password} onChange={onPasswordHandler} />
+                <br />
+                <button type="subit">
+                Login    
+                </button>                
+            </form>
+
+
         </div>
     )
 }
